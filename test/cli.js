f18812c632b3727a661a7e5bcf2b88acd954b692
Cliffano Subagio <cliffano@gmail.com>
2012-05-02 22:49:12 +1000

    Add exit_cb function for process exit handling with custom success and error callbacks.

--- test/cli.js
+++ test/cli.js
@@ -29,10 +29,45 @@ describe('cli', function () {
       assert.equal(checks.process_exit_code, 0);
     });
 
-    it('should exit with status code 1 when error exists and logs the error message', function () {
+    it('should exit with status code 1 and logs the error message when error exists', function () {
       cli.exit(new Error('some error'));
       assert.equal(checks.console_error_messages.length, 1);
       assert.equal(checks.console_error_messages[0], 'some error');
+      assert.equal(checks.process_exit_code, 1);
+    });
+  });
+
+  describe('exit_cb', function () {
+
+    it('should exit with status code 0 and logs the result when error does not exist and no success callback is specified', function () {
+      cli.exit_cb()(null, 'some success');
+      assert.equal(checks.console_log_messages.length, 1);
+      assert.equal(checks.console_log_messages[0], 'some success');
+      assert.equal(checks.process_exit_code, 0);
+    });
+
+    it('should exit with status code 1 and logs the error message when error exists and no error callback is specified', function () {
+      cli.exit_cb()(new Error('some error'));
+      assert.equal(checks.console_error_messages.length, 1);
+      assert.equal(checks.console_error_messages[0], 'some error');
+      assert.equal(checks.process_exit_code, 1);
+    });
+
+    it('should exit with status code 0 and call success callback when error does not exist and success callback is specified', function () {
+      function successCb(result) {
+        checks.success_result = result;
+      }
+      cli.exit_cb(null, successCb)(null, 'some success');
+      assert.equal(checks.success_result, 'some success');
+      assert.equal(checks.process_exit_code, 0);
+    });
+
+    it('should exit with status code 1 and call error callback when error exists and error callback is specified', function () {
+      function errorCb(err) {
+        checks.error_err = err;
+      }
+      cli.exit_cb(errorCb)(new Error('some error'));
+      assert.equal(checks.error_err.message, 'some error');
       assert.equal(checks.process_exit_code, 1);
     });
   });
