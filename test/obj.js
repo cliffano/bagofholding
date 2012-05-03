28bd995e0500adda967764e17545bb998ea452ca
Cliffano Subagio <cliffano@gmail.com>
2012-05-03 12:59:32 +1000

    Replace assert with should.

--- test/obj.js
+++ test/obj.js
@@ -2,6 +2,7 @@ var assert = require('assert'),
   bag = require('bagofholding'),
   mocha = require('mocha'),
   sandbox = require('sandboxed-module'),
+  should = require('should'),
   obj = require('../lib/obj');
 
 describe('obj', function () {
@@ -9,68 +10,68 @@ describe('obj', function () {
   describe('exist', function () {
 
     it('should return false when object is empty', function () {
-      assert.equal(obj.exist('a.b.c', {}), false);
+      obj.exist('a.b.c', {}).should.be.false;
     });
 
     it('should return false when object is undefined', function () {
-      assert.equal(obj.exist('a.b.c', undefined), false);
+      obj.exist('a.b.c', undefined).should.be.false;
     });
 
     it('should return false when dsv is empty', function () {
-      assert.equal(obj.exist('', { a: 'foo' }), false);
+      obj.exist('', { a: 'foo' }).should.be.false;
     });
 
     it('should return false when dsv is undefined', function () {
-      assert.equal(obj.exist(undefined, { a: 'foo' }), false);
+      obj.exist(undefined, { a: 'foo' }).should.be.false;
     });
 
     it('should return true when object contains a property', function () {
-      assert.equal(obj.exist('a', { a: 'foo' }), true);
+      obj.exist('a', { a: 'foo' }).should.be.true;
     });
 
     it('should return true when object contains nested properties', function () {
-      assert.equal(obj.exist('a.b', { a: { b: 'foo' } }), true);
+      obj.exist('a.b', { a: { b: 'foo' } }).should.be.true;
     });
 
     it('should return false when object does not contain nested properties', function () {
-      assert.equal(obj.exist('a.b.c', { a: { b: 'foo' } }), false);
+      obj.exist('a.b.c', { a: { b: 'foo' } }).should.be.false;
     });
   });
 
   describe('value', function () {
 
     it('should return undefined when object is empty', function () {
-      assert.equal(obj.value('a.b.c', {}), undefined);
+      should.not.exist(obj.value('a.b.c', {}));
     });
 
     it('should return undefined when object is undefined', function () {
-      assert.equal(obj.value('a.b.c', undefined), undefined);
+      should.not.exist(obj.value('a.b.c', undefined));
     });
 
     it('should return undefined when dsv is empty', function () {
-      assert.equal(obj.value('', { a: 'foo' }), undefined);
+      should.not.exist(obj.value('', { a: 'foo' }));
     });
 
     it('should return undefined when dsv is undefined', function () {
-      assert.equal(obj.value(undefined, { a: 'foo' }), undefined);
+      should.not.exist(obj.value(undefined, { a: 'foo' }));
     });
 
     it('should return value when object has one property', function () {
-      assert.equal(obj.value('a', { a: 'foo' }), 'foo');
+      obj.value('a', { a: 'foo' }).should.equal('foo');
     });
 
     it('should return value when object has nested properties', function () {
-      assert.equal(obj.value('a.b', { a: { b: 'foo' } }), 'foo');
+      obj.value('a.b', { a: { b: 'foo' } }).should.equal('foo');
     });
 
     it('should return value when object has multi nested properties', function () {
       var data = { a: { b: 'foo' }, c: 'bar' };
-      assert.equal(obj.value('a.b', data), 'foo');
-      assert.equal(obj.value('c', data), 'bar');
+      obj.value('a.b', data).should.equal('foo');
+      obj.value('c', data).should.equal('bar');
     });
 
     it('should return undefined if nested properties do not exist but the name of leaf node property exists as root property in object', function () {
-      assert.equal(obj.value('a.b.c', { a: { b: 'foo' }, c: 'bar' }), undefined);
+      should.not.exist(obj.value('a.b.c', { a: { b: 'foo' }, c: 'bar' }));
     });
   });
 });
