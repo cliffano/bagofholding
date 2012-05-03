71aa797ca86e3d0c72580763749d016a591841c8
Cliffano Subagio <cliffano@gmail.com>
2012-05-03 12:49:46 +1000

    Replace assert with should.

--- test/bagofholding.js
+++ test/bagofholding.js
@@ -1,16 +1,14 @@
 var assert = require('assert'),
   bag = require('../lib/bagofholding'),
   mocha = require('mocha'),
-  sandbox = require('sandboxed-module');
+  sandbox = require('sandboxed-module'),
+  should = require('should');
 
 describe('bagofholding', function () {
 
   it('should expose all modules', function () {
-    assert.equal(bag.cli !== undefined, true);
-    assert.equal(typeof bag.cli, 'object');
-    assert.equal(bag.mock !== undefined, true);
-    assert.equal(typeof bag.mock, 'object');
-    assert.equal(bag.obj !== undefined, true);
-    assert.equal(typeof bag.obj, 'object');
+    should.exist(bag.cli);
+    should.exist(bag.mock);
+    should.exist(bag.obj);
   });
 });
